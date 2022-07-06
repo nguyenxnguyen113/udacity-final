25910d5523f5386d0bbbb709e0e6657ef39bfdf6
nguyendev <nguyen016279@gmail.com>
2022-07-06 21:46:34 +0700

    create jwt

--- starter_code/src/main/java/com/example/demo/model/persistence/User.java
+++ starter_code/src/main/java/com/example/demo/model/persistence/User.java
@@ -13,49 +13,57 @@ import javax.persistence.Table;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-
 @Entity
 @Table(name = "user")
 public class User {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@JsonProperty
-	private long id;
-	
-	@Column(nullable = false, unique = true)
-	@JsonProperty
-	private String username;
-	
-	@OneToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "cart_id", referencedColumnName = "id")
-	@JsonIgnore
-    private Cart cart;
-	
-	public Cart getCart() {
-		return cart;
-	}
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @JsonProperty
+  private long id;
 
-	public void setCart(Cart cart) {
-		this.cart = cart;
-	}
+  @Column(nullable = false, unique = true)
+  @JsonProperty
+  private String username;
 
-	public long getId() {
-		return id;
-	}
+  @Column(nullable = false)
+  @JsonProperty
+  private String password;
 
-	public void setId(long id) {
-		this.id = id;
-	}
+  @OneToOne(cascade = CascadeType.ALL)
+  @JoinColumn(name = "cart_id", referencedColumnName = "id")
+  @JsonIgnore
+  private Cart cart;
 
-	public String getUsername() {
-		return username;
-	}
+  public Cart getCart() {
+    return cart;
+  }
 
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	
-	
-	
+  public void setCart(Cart cart) {
+    this.cart = cart;
+  }
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+  public void setPassword(String password) {
+    this.password = password;
+  }
 }
+
